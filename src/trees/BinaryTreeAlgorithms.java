acb125efa63bae8694dbc4b37d8637f7755b213e
QianruZhang78 <zhangqianru020708@gmail.com>
2020-03-10 14:03:56 +0800

    Reformat code

--- src/trees/BinaryTreeAlgorithms.java
+++ src/trees/BinaryTreeAlgorithms.java
@@ -8,8 +8,9 @@ import java.util.List;
 public class BinaryTreeAlgorithms {
     /**
      * Performs a pre-order traversal of a binary tree.
+     *
      * @param root Root of binary tree.
-     * @param <T> Type of node payload.
+     * @param <T>  Type of node payload.
      * @return A Collection containing the node payloads in traversal order.
      */
     public static <T> List<T> preOrder(BinaryNode<T> root) {
@@ -30,8 +31,9 @@ public class BinaryTreeAlgorithms {
 
     /**
      * Performs a in-order traversal of a binary tree.
+     *
      * @param root Root of binary tree.
-     * @param <T> Type of node payload.
+     * @param <T>  Type of node payload.
      * @return A Collection containing the node payloads in traversal order.
      */
     public static <T> List<T> inOrder(BinaryNode<T> root) {
@@ -52,8 +54,9 @@ public class BinaryTreeAlgorithms {
 
     /**
      * Performs a post-order traversal of a binary tree.
+     *
      * @param root Root of binary tree.
-     * @param <T> Type of node payload.
+     * @param <T>  Type of node payload.
      * @return A Collection containing the node payloads in traversal order.
      */
     public static <T> List<T> postOrder(BinaryNode<T> root) {
@@ -74,7 +77,8 @@ public class BinaryTreeAlgorithms {
 
     /**
      * Conduct a binary search on a binary search tree for a target value.
-     * @param root Root of the binary search tree.
+     *
+     * @param root  Root of the binary search tree.
      * @param value The value to search for.
      * @return The node containing the value, or null if the value is not present in the tree.
      */
@@ -94,7 +98,8 @@ public class BinaryTreeAlgorithms {
 
     /**
      * Inserts an Integer value into a Binary Search Tree.
-     * @param root Root of the binary search tree.
+     *
+     * @param root  Root of the binary search tree.
      * @param value The value to insert.
      * @return The BinaryNode containing the newly inserted value, or an existing BinaryNode with an equal value.
      */
@@ -122,6 +127,7 @@ public class BinaryTreeAlgorithms {
 
     /**
      * Determines if two BSTs are equal in value.
+     *
      * @param A Root of first tree.
      * @param B Root of second tree.
      * @return True or false depending on the equality of the two trees.
@@ -144,17 +150,17 @@ public class BinaryTreeAlgorithms {
      * Finds the path from the tree root to a target element.
      * This algorithm does NOT assume the tree is a Binary Search Tree,
      * only that it is a Binary Tree.
-     *
+     * <p>
      * Hint: This method is a bit harder than the ones above.
      * Consider implementing some TreeAlgorithms first to get some more recursion practice.
-     *
+     * <p>
      * Hint: You can use the LinkedList::addAll method to append all the contents of
      * another list to a list (like a join, but copies and is non-destructive).
      * You may also use the LinkedList::addFirst method to push to the front of the list.
      *
-     * @param root Root of the tree.
+     * @param root  Root of the tree.
      * @param value The value to search for.
-     * @param <T> The type of the value to search for.
+     * @param <T>   The type of the value to search for.
      * @return A LinkedList of Directions that lead to the target value.
      * If the target value is at the root element, return an empty list.
      * If the target value is not present in the tree, return null.
